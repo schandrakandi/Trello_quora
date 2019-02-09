cbcb4f2e8ab20f6200e941032cf6abb9bb636e64
Manasa A P <manasa.patil15@gmail.com>
2019-02-10 01:19:07 +0530

    REST1:AdminController implementation

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
@@ -1,4 +1,26 @@
 package com.upgrad.quora.api.controller;
 
+import com.upgrad.quora.api.model.UserDeleteResponse;
+import com.upgrad.quora.service.business.UserAdminBusinessService;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
 public class AdminController {
+
+    @Autowired
+    private UserAdminBusinessService userAdminBusinessService;
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/admin/user/{userId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<UserDeleteResponse> userDelete(@PathVariable("userId") final String userUuid, @RequestHeader("access-token") final String accessToken) throws AuthorizationFailedException, UserNotFoundException {
+        String uuid = userAdminBusinessService.deleteUser(userUuid,accessToken);
+        UserDeleteResponse userDeleteResponse = new UserDeleteResponse().id(uuid).status("USER SUCCESSFULLY DELETED");
+        return new ResponseEntity<UserDeleteResponse>(userDeleteResponse, HttpStatus.OK);
+    }
 }
